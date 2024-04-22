6f31705f3a8657379dcdc77b857002ba4310e1c6
May Neelon <mayflowerc10@gmail.com>
2024-04-22 17:28:01 -0400

    Finish ML09 problem

--- ML09/KMeans1.py
+++ ML09/KMeans1.py
@@ -11,7 +11,8 @@ def step_centroids(X, Y, C):
         minpos = np.argmin(dist_C)
         new_C[minpos] += coord
         groups[idx] = minpos
-    new_C /= len(X)
+    num_groups = [np.count_nonzero(groups == idx) for idx in range(int(max(groups)) + 1)]
+    new_C = new_C / np.c_[num_groups, num_groups]
     return (new_C, groups)
 
 file = "Data/clustered_data.csv"
@@ -27,9 +28,15 @@ while True:
         break
     groups = new_groups
 
-for idx in range(len(X)):
-    plt.scatter(X[idx], Y[idx], label=f"Cluster {int(groups[idx] + 1)}")
+#print(groups)
 
-plt.scatter(C[:, 0], C[:, 1], label="Centroids")
-plt.legend()
+colors = ["red", "green", "blue"]
+
+for idx in range(len(X)):
+    plt.scatter(X[idx], Y[idx], label=f"Cluster {int(groups[idx] + 1)}", color=colors[int(groups[idx])])
+
+plt.scatter(C[:, 0], C[:, 1], label="Centroids", color="yellow", marker="X", s=100)
+handles, labels = plt.gca().get_legend_handles_labels()
+by_label = dict(zip(labels, handles))
+plt.legend(by_label.values(), by_label.keys())
 plt.show()
