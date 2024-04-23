37e85d5f06e88d679f61d1f039f884a829bdbde7
May <MayflowerC10@gmail.com>
2024-04-22 23:58:50 -0400

    Add logistic regression with one feature

--- ML08/logistic_reg_1.py
+++ ML08/logistic_reg_1.py
@@ -2,6 +2,14 @@ import matplotlib.pyplot as plt
 import pandas as pd
 import numpy as np
 
+def print_cm(con_mat):
+    #print(f"{con_mat}")
+    accuracy = (con_mat[0][0] + con_mat[1][1]) / (con_mat[0][0] + con_mat[1][1] + con_mat[1][0] + con_mat[0][1]) 
+    precision = con_mat[1][1] / (con_mat[1][1] + con_mat[0][1])
+    recall = con_mat[1][1] / (con_mat[1][1] + con_mat[1][0])
+    f1 = 2 * (precision * recall) / (precision + recall)
+    print(f"TP: {con_mat[1][1]}, TN: {con_mat[0][0]}, FP: {con_mat[0][1]}, FN: {con_mat[1][0]} Accuracy: {accuracy}, precision: {precision}, recall: {recall}, f1: {f1}")
+
 def standardize_column(x, idx):
     X = np.copy(x)
     m = np.mean(X[:, idx])
@@ -13,7 +21,7 @@ def compute_loss(W, X, y):
     return sum([np.log(X[idx] @ W) * y[idx] + (1 - y[idx]) * (np.log(1 - X[idx] @ W)) for idx in range(len(y))]) / len(y)
 
 def delta_weights(X, y, W):
-    p = np.round(1 / (1 + np.exp(-1 * X.dot(W))))
+    p = 1 / (1 + np.exp(-1 * X.dot(W)))
     Dw = (p - y).dot(X)
     return Dw
 
@@ -30,15 +38,17 @@ x = np.array(data.iloc[:, 0:(len(data.columns) - 1)])
 x = np.concatenate([np.ones((x.shape[0], 1)), x], axis=1)
 #X = standardize_column(standardize_column(x, 1), 2)
 y = np.array(data[data.columns[-1]])
-biases = np.array([0.0, 0.0])
+W = np.array([0.0, 0.0])
 
 learning_rate = 0.01
 iterations = 5000
 
-biases = gradient_descent(x, y, biases, learning_rate, iterations)
-print(f"Biases = {biases}")
-p = np.round(1 / (1 + np.exp(-1 * x.dot(biases))))
-plt.scatter(x[:, 1], y, color="blue")
-plt.scatter(x[:, 1], x.dot(biases), label="Logistic Gradient Descent Fit", color="red")
-plt.legend()
-plt.show()
+W = gradient_descent(x, y, W, learning_rate, iterations)
+print(f"Weights = {W}")
+P = np.round(1 / (1 + np.exp(-1 * x.dot(W))))
+print("Can't plot data points for only one variable!")
+confusion_matrix = np.array([[0,0], [0,0]])
+for idx in range(len(y)):
+    confusion_matrix[int(P[idx])][int(y[idx])] += 1
+
+print_cm(confusion_matrix)
